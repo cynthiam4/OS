64a0d196bc04c65cf76a4e7f581175e265a0bdd1
cynthiam4 <cynthiamora97@gmail.cop>
2018-02-20 13:20:34 -0800

    Realized we DONT want example here haha

---
+++ procReport.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/module.h>
+//TODO DEFINE METHOD SIGNATURES
+
+
+//This is a linux kernel module that generates a report describing
+//the running processes on a linux kernel
+int main()
+{
+  
+   return 0;
+}
+//
+void produceReport(){
+
+}
